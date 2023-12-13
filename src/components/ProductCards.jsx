6d38464a7b59b7dae6843d7cf30205e8589f18a8
adeniji-adebanjo <aeben.adebanjo@gmail.com>
2023-12-13 14:53:56 +0100

    edited cards with props

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -2,17 +2,60 @@ import React from "react";
 import "../css/products-cards.css";
 import ProductCardsItem from "./ProductCardsItem";
 
+// const ProductCards = () => {
+//   return (
+//     <>
+//       <div className="product-cards">
+//         <ProductCardsItem />
+//         <ProductCardsItem />
+//         <ProductCardsItem />
+//         <ProductCardsItem />
+//         <ProductCardsItem />
+//       </div>
+//     </>
+//   );
+// };
+
 const ProductCards = () => {
+  const products = [
+    {
+      imageSrc:
+        "https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png",
+      title: "Dairy Products",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      imageSrc:
+        "https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png",
+      title: "Vegetables and Fruits",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      imageSrc:
+        "https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png",
+      title: "Spices and Seasoning",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      imageSrc:
+        "https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png",
+      title: "Honey",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      imageSrc:
+        "https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png",
+      title: "Flour",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+
   return (
-    <>
-      <div className="product-cards">
-        <ProductCardsItem />
-        <ProductCardsItem />
-        <ProductCardsItem />
-        <ProductCardsItem />
-        <ProductCardsItem />
-      </div>
-    </>
+    <div className="product-cards">
+      {products.map((product, index) => (
+        <ProductCardsItem key={index} {...product} />
+      ))}
+    </div>
   );
 };
 
--- src/components/ProductCardsItem.jsx
+++ src/components/ProductCardsItem.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../css/products-cards.css";
 
-const ProductCardsItem = () => {
+const ProductCardsItem = ({ imageSrc, title, description }) => {
   return (
-    <>
-      <div className="products">
-        <img
-          src="https://res.cloudinary.com/ds83mhjcm/image/upload/v1701948824/FinestMart/Dairy_tbfjb5.png"
-          alt="dairy-products"
-        />
-        <h5>Diary Products</h5>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-    </>
+    <div className="products">
+      <img src={imageSrc} alt={title} />
+      <h5>{title}</h5>
+      <p>{description}</p>
+    </div>
   );
 };
+
+ProductCardsItem.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+
 
 export default ProductCardsItem;
